6d07d6dc29228a6f472b28c9baa894a4ee1e9e20
whtsky <whtsky@vip.qq.com>
2011-11-19 23:40:10 +0800

    Add some description.

--- PyWapFetion.py
+++ PyWapFetion.py
@@ -2,7 +2,6 @@
 import cookielib
 import urllib2
 from urllib import urlencode
-from re import compile
 from types import *
 __name__ = 'PyWapFetion'
 __author__ = 'whtsky'
@@ -13,35 +12,42 @@ __license__ = 'MIT'
 class Cache:
     def __init__(self,path):
         from marshal import load
-        self.path=path
+        self.path = path
         try:
-            f=open(path,'rb')
-            self.dict=load(f)
+            f = open(path,'rb')
+            self.dict = load(f)
             f.close()
+            del f
         except:
             self.dict={}
         
     def get(self,phone):
+        #从字典中取飞信ID，成功返回ID，失败返回False
         try:
             return self.dict[phone]
         except:
             return False
             
     def put(self,phone,id):
-        self.dict[phone]=id
+        #将ID存入字典
+        self.dict[phone] = id
         
     def rm(self,phone):
+        #从字典中删除ID
         try:
             self.dict.pop[phone]
             return True
         except:
+            #如字典中没有本ID则返回False
             return False
         
     def save(self):
-        f=open(self.path,'wb')
+        #将字典保存到文件
         from marshal import dump
+        f = open(self.path,'wb')
         dump(self.dict,f)
         f.close()
+        del f
         
     def exit(self):
         self.save()
@@ -49,37 +55,45 @@ class Cache:
         del self.dict
         
 def send2self(mobile,password,message):
-    myfetion = Fetion(mobile,password)
-    myfetion.send2self(message)
-    myfetion.logout()
+    x = Fetion(mobile,password)
+    x.send2self(message)
+    x.logout()
 
 def send(mobile,password,mobile2,message):
-    myfetion = Fetion(mobile,password)
-    myfetion.send(mobile2,message)
-    myfetion.logout()
+    x = Fetion(mobile,password)
+    x.send(mobile2,message)
+    x.logout()
 
 class Fetion:
     def __init__(self,mobile,password,status='4',cachefile='Fetion.cache'):
+        #如不使用缓存，则设cachefile=False
         if cachefile:
-            self.cache=Cache(cachefile)
+            self.cache = Cache(cachefile)
         else:
-            self.cache=False
-        self.mobile=mobile
-        self.password=password
-        self.status=status
+            self.cache = False
+        #在有缓存的情况下，创建对象时不载入正则，提高速度。           
+        self.idfinder = False
+            
+        self.mobile = mobile
+        self.password = password
+        self.status = status
+        
         self._login()
-        self.idfinder = compile('touserid=(\d*)')
         
     def send2self(self,message):
+        #发送给自己
         data = urlencode({'msg':message})
         req = urllib2.Request('http://f.10086.cn/im/user/sendMsgToMyselfs.action',data)
         return '成功' in self.opener.open(req).read()
         
     def send(self,mobile,message):
         if type(mobile) != StringType:
+            #构建一个字典并将每一个号码的发送结果存入字典
             results = {}
             for x in mobile:
-                results[x] = self._send(mobile,message)
+                #实际的发送操作在_send中
+                results[x] = self._send(x,message)
+            #返回字典
             return results
         else:
             return self._send(mobile,message)
@@ -92,22 +106,31 @@ class Fetion:
         return '成功' in self.opener.open(req).read()
         
     def logout(self):
+        #退出飞信，否则可能会影响正常短信收发
         self.opener.open('http://f.10086.cn/im/index/logoutsubmit.action')
+        #这些都是可能不存在的东西，防止异常
+        try:
+            del self.idfinder
+            self.cache.exit()
+            del self.cache
+        except:
+            pass
+        #扫尾工作
         del self.opener
         del self.mobile
         del self.password
         del self.status
-        del self.idfinder
-        self.cache.exit()
-        del self.cache
     
     def _send(self,mobile,message):
         if mobile == self.mobile:
+            #如果传入的手机号是自己的手机号，则调用send2self().
             return self.send2self(message)
-        return self.send2id(self.findid(mobile),message)
+        else:    
+            return self.send2id(self.findid(mobile),message)
 
     def _login(self):
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()), urllib2.HTTPHandler)
+        #登陆并保存cookies.
         data = urlencode({'m':self.mobile,
                          'pass':self.password,
                          'loginstatus':self.status})
@@ -115,20 +138,32 @@ class Fetion:
         self.opener.open(req) 
         
     def _getid(self,mobile):
+        #如果尚未构建正则表达式对象，则创建
+        if self.idfinder == False:
+            from re import compile
+            self.idfinder = compile('touserid=(\d*)')
+            
+        #获得HTML页面
         data = urlencode({'searchText':mobile})
         req = urllib2.Request('http://f.10086.cn/im/index/searchOtherInfoList.action',data)
         htm = self.opener.open(req).read()
+        
+        #正则匹配飞信ID
         result = self.idfinder.findall(htm)
+        
+        #找到返回ID，否则返回False
         if len(result) > 0:
             return result[0] 
         return False
         
     def findid(self,mobile):
         if self.cache:
+            #如果开启缓存（默认开启），则查找缓存文件
             id = self.cache.get(mobile)
             if id:
                 return id
             else:
+                #缓存中没有，获取ID并存入。
                 id = self._getid(mobile)
                 self.cache.put(mobile,id)
                 return id
