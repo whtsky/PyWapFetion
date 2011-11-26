41de28e3f43ca0780d2f1d19e1c04762dc604493
whtsky <whtsky@vip.qq.com>
2011-11-26 21:55:41 +0800

    Use setuptools(pip) to install.

--- setup.py
+++ setup.py
@@ -1,13 +1,48 @@
-#coding=utf-8
-from distutils.core import setup
+﻿#coding=utf-8
+from setuptools import setup
 import PyWapFetion
 
 setup(
 name = PyWapFetion.__name__,
 version = PyWapFetion.__version__,
-description = 'A simple python lib for WapFetion',
-license = PyWapFetion.__license__,
-author = PyWapFetion.__author__,
-url = PyWapFetion.__website__,
+
 py_modules=['PyWapFetion'],
+keywords = 'library mobile fetion',
+author = PyWapFetion.__author__,
+author_email = 'whtsky@vip.qq.com',
+
+url = PyWapFetion.__website__,
+download_url = 'https://github.com/whtsky/PyWapFetion/zipball/master',
+description = 'A simple python lib for WapFetion',
+long_description = '''
+Fetion SDK for Python.
+
+PyWapFetion is a useful Fetion Library for Python made by .It's very easy to use.
+
+Go to https://github.com/whtsky/PyWapFetion for more information.
+
+* Cache support
+* Auto keep alive support
+* NO CAPTCHA during using it
+* Supports Python 2.5 - 2.7
+* MIT LICENSE
+
+THIS IS A THRID PARTY SDK,USE AT YOUR RISK.
+''',
+license = PyWapFetion.__license__,
+classifiers = [
+'Development Status :: 4 - Beta',
+'Environment :: Console',
+'Intended Audience :: Developers',
+'License :: OSI Approved :: MIT License',
+'Natural Language :: English',
+'Natural Language :: Chinese (Simplified)',
+'Operating System :: OS Independent',
+'Programming Language :: Python :: 2',
+'Programming Language :: Python :: 2.5',
+'Programming Language :: Python :: 2.6',
+'Programming Language :: Python :: 2.7',
+'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+'Topic :: Utilities'
+],
 )
