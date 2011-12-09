6950120c10ff2c8c06830c0c2a0c5f581fba1ec2
whtsky <whtsky@vip.qq.com>
2011-12-09 21:30:08 +0800

    Support Chinese in Cache.

--- PyWapFetion/Cache.py
+++ PyWapFetion/Cache.py
@@ -1,5 +1,7 @@
 ﻿#coding=utf-8
 from marshal import dump,load
+from hashlib import md5 as GETMD5
+md5 = lambda x:GETMD5(x).hexdigest()
 class Cache(object):
     def __init__(self,path):
         self.path = path
@@ -12,7 +14,7 @@ class Cache(object):
             f.close()
             del f
             
-    __getitem__ = get        = lambda self,phone:self.dict.get(phone)
-    __setitem__ = setdefault = lambda self,phone,id:self.dict.setdefault(phone,id)
-    __delitem__ = pop        = lambda self,k:self.dict.pop(k,None)
-    __del__     = save       = lambda self:dump(self.dict,open(self.path,'wb'))    
+    __getitem__ = get  = lambda self,k:self.dict.get(k) if k.isdigit() else self.dict.get(md5(k))
+    __setitem__        = lambda self,k,id:self.dict.__setitem__(k,id) if k.isdigit() else self.dict.__setitem__(md5(k),id)
+    __delitem__ = pop  = lambda self,k:self.dict.pop(k,None) if k.isdigit() else self.dict.pop(md5(k),None)
+    __del__     = save = lambda self:dump(self.dict,open(self.path,'wb'))    
